a395c01e17be95798adb818a630a5ba3b4475986
Ariel Berardi <ariel.berardi95@gmail.com>
2023-03-19 16:39:30 -0300

    Add Icons to the app

--- lib/main.dart
+++ lib/main.dart
@@ -4,14 +4,14 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.blueGrey,
+        backgroundColor: Colors.black,
         appBar: AppBar(
           title: const Text('I Am Rich'),
-          backgroundColor: Colors.blueGrey[900],
+          backgroundColor: Colors.blueGrey[800],
         ),
         body: const Center(
           child: Image(
-            image: NetworkImage('https://camo.githubusercontent.com/6e72ed7c011f9206dc0c68113a1572e5b5ab8c4e954a6840435c7998fe00cb8c/68747470733a2f2f63646e2e63756c746f666d61632e636f6d2f77702d636f6e74656e742f75706c6f6164732f323030382f30382f706f73742d323438362d696d6167652d34376435316236363134613633626631346662373965343135316265636464392e6a7067'),
+            image: AssetImage('images/diamond.png'),
           ),
         ),
       ),
